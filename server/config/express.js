9f085eb3ab0631f6da37730216574ebba26733f8
Georgi Yonchev <georgi.d.yonchev@abv.bg>
2018-09-07 16:18:13 +0200

    config session and cookie parser

--- server/config/express.js
+++ server/config/express.js
@@ -3,12 +3,19 @@
 let express = require('express'),
     stylus = require('stylus'),
     bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    passport = require('passport'),
     path = require('path');
 
 module.exports = function (app, config) {
     app.set('view engine', 'jade');
     app.set('views', path.join(config.rootPath, '/server/views'));
-
+    app.use(cookieParser());
+    app.use(session({
+        secret: 'magic unicorns and tomorrowland',
+        resave: true,
+        saveUninitialized: true}));
     app.use(bodyParser.urlencoded(
         {
         extended: true
@@ -24,5 +31,7 @@ module.exports = function (app, config) {
         }
     ));
 
+    app.use(passport.initialize());
+    app.use(passport.session());
     app.use(express.static(path.join(config.rootPath, 'public')));
 };
